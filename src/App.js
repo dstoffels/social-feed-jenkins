75e9765a48725c7c0e1d98a50bdf4e979b5f5f36
dstoffels <dan.stoffels@gmail.com>
2023-07-05 13:08:04 -0500

    refactor: updated index.html + app.js

--- src/App.js
+++ src/App.js
@@ -5,24 +5,30 @@ import Navigation from './Components/Navigation/Navigation';
 import './App.css';
 
 function App() {
-  const [posts, setPosts] = useState([{name: 'David Lagrange', body: `I recently went into the woods to search for the oldest tree I could find. To my luck I 
+	const [posts, setPosts] = useState([
+		{
+			name: 'David Lagrange',
+			body: `I recently went into the woods to search for the oldest tree I could find. To my luck I 
   found a very old hemlock around 200 - 250 years old. Let me know if you would like 
-  me to show it to you!`, liked: 0}]);
+  me to show it to you!`,
+			liked: 0,
+		},
+	]);
 
-  function addNewPost(newPost){
-    let tempPosts = [...posts, newPost];
-    setPosts(tempPosts);
-  }
+	function addNewPost(newPost) {
+		let tempPosts = [newPost, ...posts];
+		setPosts(tempPosts);
+	}
 
-  return (
-    <div>
-      <Navigation />
-      <div className='grid-rules-app' style={{'background-color': '#e2f2ee'}}>
-        <CreatePost addNewPost={addNewPost} />
-        <DisplayPosts posts={posts} />
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<Navigation />
+			<div className="grid-rules-app" style={{ 'background-color': '#e2f2ee' }}>
+				<CreatePost addNewPost={addNewPost} />
+				<DisplayPosts posts={posts} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
