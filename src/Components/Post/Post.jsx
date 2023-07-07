ce86620d9ff960158642834dda55c7d47ef23303
dstoffels <dan.stoffels@gmail.com>
2023-07-07 09:27:42 -0500

    feat: updated like/dislike btns

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,40 +1,43 @@
-import React, {useState, useEffect } from 'react';
-import {ReactComponent as ThumbsDown} from '../../SVG/thumbs-down-solid.svg';
-import {ReactComponent as ThumbsUp} from '../../SVG/thumbs-up-solid.svg';
+import React, { useState, useEffect } from 'react';
+import { ReactComponent as ThumbsDown } from '../../SVG/thumbs-down-solid.svg';
+import { ReactComponent as ThumbsUp } from '../../SVG/thumbs-up-solid.svg';
 import './Post.css';
 
 const Post = (props) => {
-    const [thumbsDownColor, setThumbsDownColor] = useState('grey');
-    const [thumbsUpColor, setThumbsUpColor] = useState('grey');
+	const [liked, setLiked] = useState(false);
+	const [disliked, setDisliked] = useState(false);
 
-    useEffect(() => {
-        
-    }, [props.post]);
+	function handleLike(e) {
+		setLiked(!liked);
+		setDisliked(false);
+	}
 
-    function changeThumbColors(colorNumber){
+	function handleDislike(e) {
+		setDisliked(!disliked);
+		setLiked(false);
+	}
 
-        if(colorNumber === 1){
-            setThumbsDownColor('grey')
-            setThumbsUpColor('green')
-        } else if (colorNumber === -1){
-            setThumbsDownColor('red')
-            setThumbsUpColor('grey')
-        }else{
-            setThumbsDownColor('grey')
-            setThumbsUpColor('grey')
-        }
-    }
+	return (
+		<div>
+			<p style={{ fontWeight: 'bold' }}>{props.post.name}</p>
+			<p>{props.post.body}</p>
+			<span className="icons-span">
+				<ThumbsUp
+					className="like-btn"
+					fill={liked ? 'green' : 'grey'}
+					height="1.5em"
+					style={{ 'margin-right': '1em' }}
+					onClick={handleLike}
+				/>
+				<ThumbsDown
+					className="like-btn"
+					fill={disliked ? 'red' : 'grey'}
+					height="1.5em"
+					onClick={handleDislike}
+				/>
+			</span>
+		</div>
+	);
+};
 
-    return ( 
-        <div>
-            <p style={{'fontWeight': 'bold'}}>{props.post.name}</p>
-            <p>{props.post.body}</p>
-            <span className='icons-span'>
-                <ThumbsUp fill={thumbsUpColor} height='1.5em' style={{'margin-right': '1em'}} onClick={() => changeThumbColors(1)}/>
-                <ThumbsDown fill={thumbsDownColor} height='1.5em' onClick={() => changeThumbColors(-1)}/>
-            </span>
-        </div>
-     );
-}
- 
 export default Post;
